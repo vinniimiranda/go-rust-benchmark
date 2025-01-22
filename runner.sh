93c65cd3d767d1f92286c5ad8a94c0db9ce15db5
vinniimiranda <vinniimiranda@gmail.com>
2025-01-21 22:48:50 -0300

    feat: add datetime for results and add llama analysis

--- runner.sh
+++ runner.sh
@@ -27,21 +27,27 @@ monitor_docker_stats() {
 }
 
 
+DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+wait
+echo "DATE: $DATE"
+mkdir -p "./results/$DATE"
+
+
 # RUST k6 load test
 echo "STARTING RUST LOAD TEST"
-monitor_docker_stats "rust-api" "./results/rust_docker_stats.md" &
-k6 run load_test/load_test_rust.js >> ./results/rust_k6_results.txt
-awk '/status é/,/running/' ./results/rust_k6_results.txt > ./results/rust_k6_results_metrics.txt
+monitor_docker_stats "rust-api" "./results/${DATE}/rust_docker_stats.md" &
+k6 run load_test/load_test_rust.js >> ./results/${DATE}/rust_k6_results.txt
+awk '/status é/,/running/' ./results/${DATE}/rust_k6_results.txt > ./results/${DATE}/rust_k6_results_metrics.txt
 wait
 echo "FINISHED RUST LOAD TEST"
 sleep 10
 
 
-# # GO k6 load test
+# GO k6 load test
 echo "STARTING GO LOAD TEST"
-monitor_docker_stats "go-api" "./results/go_docker_stats.md" &
-k6 run load_test/load_test_go.js >> ./results/go_k6_results.txt
-awk '/status é/,/running/' ./results/go_k6_results.txt > ./results/go_k6_results_metrics.txt
+monitor_docker_stats "go-api" "./results/${DATE}/go_docker_stats.md" &
+k6 run load_test/load_test_go.js >> ./results/${DATE}/go_k6_results.txt
+awk '/status é/,/running/' ./results/${DATE}/go_k6_results.txt > ./results/${DATE}/go_k6_results_metrics.txt
 wait
 echo "FINISHED GO LOAD TEST"
 
@@ -58,22 +64,30 @@ sleep 10
 
 
 # Write final results file
-echo "# Final Results for execution" > ./results/results.md
-echo "## RUST" >> ./results/results.md
-echo '### K6' >> ./results/results.md
-echo "\`\`\`rust" >> ./results/results.md
-cat ./results/rust_k6_results_metrics.txt >> ./results/results.md
-echo "\`\`\`" >> ./results/results.md
-echo '### Metrics' >> ./results/results.md
-cat ./results/rust_docker_stats.md >> ./results/results.md
-echo "\n\n\n" >> ./results/results.md
+echo "# Final Results for execution" > ./results/${DATE}/final_results.md
+echo "## RUST" >> ./results/${DATE}/final_results.md
+echo '### K6' >> ./results/${DATE}/final_results.md
+echo "\`\`\`rust" >> ./results/${DATE}/final_results.md
+cat ./results/${DATE}/rust_k6_results_metrics.txt >> ./results/${DATE}/final_results.md
+echo "\`\`\`" >> ./results/${DATE}/final_results.md
+echo '### Metrics' >> ./results/${DATE}/final_results.md
+cat ./results/${DATE}/rust_docker_stats.md >> ./results/${DATE}/final_results.md
+echo "\n\n\n" >> ./results/${DATE}/final_results.md
 
-echo "## GO" >> ./results/results.md
-echo '### K6' >> ./results/results.md
-echo "\`\`\`go" >> ./results/results.md
-cat ./results/go_k6_results_metrics.txt >> ./results/results.md
-echo "\`\`\`" >> ./results/results.md
-echo '### Metrics' >> ./results/results.md
-cat ./results/go_docker_stats.md >> ./results/results.md
+echo "## GO" >> ./results/${DATE}/final_results.md
+echo '### K6' >> ./results/${DATE}/final_results.md
+echo "\`\`\`go" >> ./results/${DATE}/final_results.md
+cat ./results/${DATE}/go_k6_results_metrics.txt >> ./results/${DATE}/final_results.md
+echo "\`\`\`" >> ./results/${DATE}/final_results.md
+echo '### Metrics' >> ./results/${DATE}/final_results.md
+cat ./results/${DATE}/go_docker_stats.md >> ./results/${DATE}/final_results.md
 echo "\n\n"
 
+echo '## Database results' >> ./results/${DATE}/final_results.md
+PGPASSWORD=B3pfzQDJhFgKM6B4XTJ9bs1RPlPLsrD9RJZkVphBdsjlCapR29OAgIEZ7MuYc3bu psql -h localhost -p 5432 -d app -U app -c "select distinct source, count(source) from transactions group by source;" >> ./results/${DATE}/final_results.md
+
+echo "FINISHED"
+
+echo "##  AI Analysis" >> ./results/${DATE}/final_results.md
+
+cat ./results/${DATE}/final_results.md | ollama run llama3.2  "Crie uma breve análise dos principais resultados do Benchmark entre Golang e Rust" >> ./results/${DATE}/final_results.md
